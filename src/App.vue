4c612a22a033b6c7a31cf36030f4e1d32c129802
Tiago Matos <tiago@tiagomatos.com>
2023-03-21 11:50:57 +1100

    Aula 07

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-app>
+  <v-app theme="dark">
     <v-app-bar flat class="border-b">
-      <v-app-bar-title>My app</v-app-bar-title>
+      <v-app-bar-title>Meu app</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
@@ -9,10 +9,74 @@
         <h1>Dashboard</h1>
 
         <v-row>
-          <v-col cols="12" md="4" lg="3">1</v-col>
-          <v-col cols="12" md="4" lg="3">1</v-col>
-          <v-col cols="12" md="4" lg="3">1</v-col>
-          <v-col cols="12" md="4" lg="3">1</v-col>
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://images.pexels.com/photos/13435510/pexels-photo-13435510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              >
+                <v-card-title>Top 10 Praia na Bahia</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-3">Salvador</v-card-subtitle>
+
+              <v-card-text>
+                <div>Rio vermelho</div>
+
+                <div>Lorem ipsum dolor sit amet</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://images.pexels.com/photos/13435510/pexels-photo-13435510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              >
+                <v-card-title>Top 10 Praia na Bahia</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-3">Salvador</v-card-subtitle>
+
+              <v-card-text>
+                <div>Rio vermelho</div>
+
+                <div>Lorem ipsum dolor sit amet</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://images.pexels.com/photos/13435510/pexels-photo-13435510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              >
+                <v-card-title>Top 10 Praia na Bahia</v-card-title>
+              </v-img>
+
+              <v-card-subtitle class="pt-3">Salvador</v-card-subtitle>
+
+              <v-card-text>
+                <div>Rio vermelho</div>
+
+                <div>Lorem ipsum dolor sit amet</div>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
