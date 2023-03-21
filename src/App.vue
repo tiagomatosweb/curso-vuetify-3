20392ccb5ede6784c3e479444d194b1f1b111b50
Tiago Matos <tiago@tiagomatos.com>
2023-03-21 11:43:34 +1100

    Aula 03

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,32 @@
     </v-app-bar>
 
     <v-main>
+      <v-container>
+        <h1 class="text-h6">Meu app</h1>
+        <h2>Meu app</h2>
+        <h3>Meu app</h3>
+        <h4>Meu app</h4>
+        <h5>Meu app</h5>
+        <h6>Meu app</h6>
+        <br><br>
+        <div class="text-subtitle-1">Subtitle 1</div>
+        <div class="text-caption">Caption</div>
+
+        <div class="d-flex justify-space-between">
+          <div
+            style="width: 100px; height: 100px;"
+            class="pa-4 border rounded-lg elevation-2"
+          >teste</div>
+          <div
+            style="width: 100px; height: 100px;"
+            class="pa-4 border rounded-lg elevation-2"
+          >teste</div>
+          <div
+            style="width: 100px; height: 100px;"
+            class="pa-4 border rounded-lg elevation-2"
+          >teste</div>
+        </div>
+      </v-container>
       <HelloWorld />
     </v-main>
   </v-app>
